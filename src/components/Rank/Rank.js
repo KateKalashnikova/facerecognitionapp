406cbc82246c0a12233fd70e648f3b45918315db
Kate_Kalashnikova <68355941+KateKalashnikova@users.noreply.github.com>
2021-03-18 22:20:22 -0600

    connected to server and to db

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({ name, entries }) => {
 	return (
 		<div>
 			<div className='white i f3'>
-				{'Kate, your current rank is'}
+				{`${name}, your current entry count is `}
 			</div>
 			<div className='white i f2'>
-				{'3'}
+				{entries}
 			</div>
 		</div>
 	);
